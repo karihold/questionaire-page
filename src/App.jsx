6400a3eaa357a3a3ef7f23114e905a0763ca7b0e
karihold <holdcroftkari@gmail.com>
2021-10-02 21:47:15 +0200

    Added result page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "landmarks/footer/footer";
 //Pages
 import Frontpage from "pages/frontpage/frontpage";
 import Questions from 'pages/questions/questions';
+import Result from 'pages/result/result';
 //Styles
 import "scss/base.scss";
 
@@ -21,6 +22,9 @@ const App = () => {
         </Route>
         <Route path="/questions">
           <Questions/>
+        </Route>
+        <Route path="/result">
+          <Result/>
         </Route>
       </Switch>
       <Footer />
