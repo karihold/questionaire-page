de8ecc9753baf2de732303176df7fb4feafe5541
karihold <holdcroftkari@gmail.com>
2021-10-02 23:05:37 +0200

    Added routes to frontpage

--- src/js/pages/frontpage/frontpage.jsx
+++ src/js/pages/frontpage/frontpage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Routes from 'routes/routes';
 import './frontpage.scss';
 
 const Frontpage = () => {
   return (
     <>
-      <Link to="/questions">START</Link>
+      <Link to={Routes.Questions.Name}>START</Link>
     </>
   );
 };
