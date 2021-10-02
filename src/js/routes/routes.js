9972247b6c193223e446b80b326433e68639d07e
karihold <holdcroftkari@gmail.com>
2021-10-02 22:54:30 +0200

    Added page routes

---
+++ src/js/routes/routes.js
@@ -0,0 +1,10 @@
+const QuestionsRoute = '/questions';
+
+export default {
+  Home: '/',
+  Name: `${QuestionsRoute}/name`,
+  FavoriteColor: `${QuestionsRoute}/favorite-color`,
+  ColorReason: `${QuestionsRoute}/color-reason`,
+  ColorLoveScale: `${QuestionsRoute}/color-love-scale`,
+  Result: '/result',
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const srcPath = path.resolve(__dirname, "./src");
-const distPath = path.resolve(__dirname, "./dist");
+const srcPath = path.resolve(__dirname, './src');
+const distPath = path.resolve(__dirname, './dist');
 
 module.exports = (env) => ({
-  entry: path.resolve(srcPath, "App.jsx"),
+  entry: path.resolve(srcPath, 'App.jsx'),
   output: {
     path: distPath,
-    filename: "js/[name].js",
+    filename: 'js/[name].js',
   },
   mode: env.mode,
-  target: "web",
-  devtool: env.mode === "production" ? "eval-source-map" : false,
+  target: 'web',
+  devtool: env.mode === 'production' ? 'eval-source-map' : false,
   resolve: {
     alias: {
-      components: path.resolve(srcPath, "js/components"),
-      pages: path.resolve(srcPath, "js/pages"),
-      landmarks: path.resolve(srcPath, "js/landmarks"),
-      scss: path.resolve(srcPath, "scss/"),
-      assets: path.resolve(srcPath, "assets/"),
-      variables: path.resolve(srcPath, "scss/variables")
+      components: path.resolve(srcPath, 'js/components'),
+
+      pages: path.resolve(srcPath, 'js/pages'),
+      landmarks: path.resolve(srcPath, 'js/landmarks'),
+      routes: path.resolve(srcPath, 'js/routes'),
+      scss: path.resolve(srcPath, 'scss/'),
+      assets: path.resolve(srcPath, 'assets/'),
+      variables: path.resolve(srcPath, 'scss/variables'),
     },
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new CleanWebpackPlugin({ verbose: true }),
-    new MiniCssExtractPlugin({ filename: "css/[name].css" }),
+    new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
     new HtmlWebpackPlugin({
-      template: path.resolve(srcPath, "./index.html"),
-      inject: "body",
+      template: path.resolve(srcPath, './index.html'),
+      inject: 'body',
     }),
   ],
   module: {
@@ -39,26 +41,21 @@ module.exports = (env) => ({
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.(scss|sass)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[folder]/[name].[ext]",
-              outputPath: "assets/",
-              publicPath: "/assets",
+              name: '[folder]/[name].[ext]',
+              outputPath: 'assets/',
+              publicPath: '/assets',
             },
           },
         ],
@@ -70,6 +67,6 @@ module.exports = (env) => ({
     writeToDisk: true,
     contentBase: distPath,
     historyApiFallback: true,
-    port: "8080",
+    port: '8080',
   },
 });
