cc1467399303902192dce835ae26d1dd92b4c51b
karihold <holdcroftkari@gmail.com>
2021-10-02 22:50:35 +0200

    Fixed formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 //React
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //Landmarks
-import Header from "landmarks/header/header";
-import Footer from "landmarks/footer/footer";
+import Header from 'landmarks/header/header';
+import Footer from 'landmarks/footer/footer';
 //Pages
-import Frontpage from "pages/frontpage/frontpage";
+import Frontpage from 'pages/frontpage/frontpage';
 import Questions from 'pages/questions/questions';
 import Result from 'pages/result/result';
 //Styles
-import "scss/base.scss";
+import 'scss/base.scss';
 
 const App = () => {
   return (
@@ -20,11 +20,11 @@ const App = () => {
         <Route exact path="/">
           <Frontpage />
         </Route>
-        <Route path="/questions">
-          <Questions/>
+        <Route path="/questions/:step">
+          <Questions />
         </Route>
         <Route path="/result">
-          <Result/>
+          <Result />
         </Route>
       </Switch>
       <Footer />
@@ -32,4 +32,4 @@ const App = () => {
   );
 };
 
-render(<App />, document.querySelector("#render-target"));
+render(<App />, document.querySelector('#render-target'));
--- src/js/pages/frontpage/frontpage.jsx
+++ src/js/pages/frontpage/frontpage.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import "./frontpage.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './frontpage.scss';
 
 const Frontpage = () => {
   return (
