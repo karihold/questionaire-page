f477cb7d2e101699834807168fdd556921bd18cf
karihold <holdcroftkari@gmail.com>
2021-10-02 23:06:00 +0200

    Added question steps to questions page

--- src/js/pages/questions/questions.jsx
+++ src/js/pages/questions/questions.jsx
@@ -1,10 +1,43 @@
-import React from "react";
-import "./questions.scss";
+import React from 'react';
+import { Link, useRouteMatch, useParams } from 'react-router-dom';
+import Routes from 'routes/routes';
+import './questions.scss';
 
 const Questions = () => {
+  const { step } = useParams();
+
+  const isAtNameStep = useRouteMatch(Routes.Questions.Name);
+  const isAtFavoriteColorStep = useRouteMatch(Routes.Questions.FavoriteColor);
+  const isAtColorReasonStep = useRouteMatch(Routes.Questions.ColorReason);
+  const isAtColorLoveScaleStep = useRouteMatch(Routes.Questions.ColorLoveScale);
+
   return (
     <>
-      <h1>Questions</h1>
+      {isAtNameStep && (
+        <>
+          <h1>{step}</h1>
+
+          <Link to={Routes.Questions.FavoriteColor}>NEXT</Link>
+        </>
+      )}
+      {isAtFavoriteColorStep && (
+        <>
+          <h1>{step}</h1>
+          <Link to={Routes.Questions.ColorReason}>NEXT</Link>
+        </>
+      )}
+      {isAtColorReasonStep && (
+        <>
+          <h1>{step}</h1>
+          <Link to={Routes.Questions.ColorLoveScale}>NEXT</Link>
+        </>
+      )}
+      {isAtColorLoveScaleStep && (
+        <>
+          <h1>{step}</h1>
+          <Link to={Routes.Result}>NEXT</Link>
+        </>
+      )}
     </>
   );
 };
