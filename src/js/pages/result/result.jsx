1d94cacf7dded3a3e7f44ffd8b55e53a5b83b030
karihold <holdcroftkari@gmail.com>
2021-10-03 16:53:12 +0200

    Updated result page styling

--- src/js/pages/result/result.jsx
+++ src/js/pages/result/result.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useQuestionsContext } from 'context/questions-context';
+import ResultAnswer from 'components/result-answer/result-answer';
 import QuestionNames from 'constants/question-names';
 import './result.scss';
 
@@ -7,13 +8,21 @@ const Result = () => {
   const { answers } = useQuestionsContext();
 
   return (
-    <>
-      <h1>Result</h1>
-      <p>{answers[QuestionNames.Name]}</p>
-      <p>{answers[QuestionNames.FavoriteColor]}</p>
-      <p>{answers[QuestionNames.ColorReason]}</p>
-      <p>{answers[QuestionNames.ColorLoveScale]}</p>
-    </>
+    <div className="results-wrapper">
+      <h1 className="result-h1">Result</h1>
+      <dl>
+        <ResultAnswer question="What’s your name?" answer={answers[QuestionNames.Name]} />
+        <ResultAnswer question="What’s your favorite color?" answer={answers[QuestionNames.FavoriteColor]} />
+        <ResultAnswer
+          question={`Why is ${answers[QuestionNames.FavoriteColor]} your favorite color?`}
+          answer={answers[QuestionNames.ColorReason]}
+        />
+        <ResultAnswer
+          question={`How much do you love the color ${answers[QuestionNames.ColorLoveScale]} from 1-100?`}
+          answer={answers[QuestionNames.ColorLoveScale]}
+        />
+      </dl>
+    </div>
   );
 };
 
