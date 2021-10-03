c0d5ddeb2aaeb092d1c4b8df5b6bb838df640d12
karihold <holdcroftkari@gmail.com>
2021-10-03 10:23:53 +0200

    added question context

---
+++ src/js/context/questions-context.jsx
@@ -0,0 +1,23 @@
+import React, { useContext, createContext, useState } from 'react';
+import QuestionNames from 'constants/question-names';
+
+const defaultAnswers = {
+  [QuestionNames.Name]: '',
+  [QuestionNames.FavoriteColors]: '',
+  [QuestionNames.ColorReason]: '',
+  [QuestionNames.ColorLoveScale]: 0,
+};
+
+const QuestionsContext = createContext({});
+
+export const useQuestionsContext = () => useContext(QuestionsContext);
+
+export const QuestionsProvider = ({ children }) => {
+  const [answers, setAnswers] = useState(defaultAnswers);
+
+  function setQuestionAnswer({ question, answer }) {
+    setAnswers((prevQuestions) => ({ ...prevQuestions, [question]: answer }));
+  }
+
+  return <QuestionsContext.Provider value={{ answers, setQuestionAnswer }}>{children}</QuestionsContext.Provider>;
+};
